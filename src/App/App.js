22bb9f4164788473a2ebdad7178bf602c4cf80d5
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-11 20:45:19 -0600

    working on student cards

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-danger">Help Me</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import studentsData from '../helpers/data/studentsData';
+
+
+class App extends React.Component {
+  state = {
+    students: [],
+  }
+
+
+  componentDidMount() {
+    const students = studentsData.getStudents();
+    this.setState({ students });
+
+    const liveStudents = studentsData.livingStudents();
+    this.setState({ liveStudents });
+
+    const deadStudents = studentsData.dearlyBeloved();
+    this.setState({ deadStudents });
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
