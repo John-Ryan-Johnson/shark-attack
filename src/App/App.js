930fe4115d08daa7649c2e0d2de07e0460075ee1
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-12 12:21:49 -0600

    cards printing to shark tank

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,15 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import studentsData from '../helpers/data/studentsData';
+import SharkTank from '../components/SharkTank/SharkTank';
+import GraveYard from '../components/Graveyard/Graveyard';
 
 
 class App extends React.Component {
   state = {
     students: [],
+    liveStudents: [],
+    deadStudents: [],
   }
 
 
@@ -25,7 +29,16 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-
+        <div>
+          <div className="col-6">
+            <h1 className="text-center">Shark Tank</h1>
+            <SharkTank liveStudents={this.state.liveStudents} />
+          </div>
+          <div className="col-6">
+            <h1 className="text-center">Grave Yard</h1>
+            <GraveYard deadStudents={this.state.deadStudents} />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/LiveStudent/LiveStudent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import studentShape from '../../helpers/propz/studentShape';
+
+class LiveStudent extends React.Component {
+  static propTypes = {
+    liveStudents: studentShape.studentShape,
+  }
+
+  render() {
+    const { student } = this.props;
+    return (
+      <div className="card col-6">
+        <div className="card-body">
+          <h5 className="card-title">{student.firstName} {student.lastName}</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LiveStudent;
